bfbeeea3cd82f25f9f1b7d66828fffb26a401db0
Chima Okwara <43557896+ChiAlpha@users.noreply.github.com>
2020-04-23 18:07:33 +0100

    Add files via upload

---
+++ include/functions.h
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include <time.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <string.h>
+
+LiquidCrystal lcd (14,15,16,17,18,19);                      //pins for the lcd screen
+
+String confirmation, redo;                                   //To get feedback from user
+char firstLetter, secondLetter, thirdLetter;                //the three letters of the Word
+char Word[4];                                             //Three letter Word generated.
+char alphabets[] = {"aeiouybcdfghjklmnpqrstvwxz"};          //Alphabets to choose from
+int count (0);
+
+
+inline char generateLetter()          //Function to generate a random character
+{
+  time_t sec;
+  time(&sec);
+  long secc = (long) sec;
+    randomSeed(secc);
+    return alphabets[(random()%26+1)-1];      //Generate a random number between 0 and 25
+                                              //since the elements in alphabets[] are labelled 0 to 25
+}
+
+
+inline void confirm()                       //Function to confirm if Word exists
+{
+  confirmation=Serial.readString();         //Reads a string from the serial monitor
+
+  if (confirmation=="y"||confirmation=="Y")
+  {
+    lcd.setCursor(13,1);
+    lcd.print("yes");
+    Serial.println("yes");
+    count++;
+  }
+
+  else
+  if (confirmation=="n"||confirmation=="N")
+  {    // lcd.print("no");
+    lcd.setCursor(13,1);
+    lcd.print("no");
+    Serial.println("no");
+  }
+
+  else
+    Serial.println("No valid response");
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,109 @@
+///************ Arduino-based three-letter Word generator*****************************/////////
+///Written by Chimaroke R. Okwara (AlphaChi)***************************************************
+///Copyright (c) 03 Apr., 2020; Eichen(R) Technologies*****************************************
+///Tested on Arduino Uno. Written using PlatformIO*********************************************
+///All rights reserved*************************************************************************
+///Modifications to be done:      **************************************************///////////
+///*Improve the code so  that at least one of the letters must always be a vowel**************
+///*******************************************************************************************
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <Arduino.h>
+#include <time.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <string.h>
+#include "functions.h"
+
+// extern int count;                                          //To count the number of correct Words.
+//
+//
+// extern char Word[4];                                             //Three letter Word generated.
+// extern char alphabets[];                                          //Alphabets to choose from
+
+
+void setup()
+
+{
+///////*************BEGINNING OF SETUP ROUTINE*******************////////////////////////////////
+    //Prepares the serial monitor and the lcd screen
+    Serial.begin(9600);
+    lcd.begin(16, 2);
+    lcd.clear();
+
+    //print welcome message onto the lcd screen:
+    lcd.setCursor(3, 0);
+    lcd.print("Eichen(R)");
+    delay(1000);
+    lcd.setCursor(0, 1);
+    lcd.print("Three Letter Word Generator");
+    delay(500);
+
+    for (int i (0); i<11; ++i)  //To scroll the second part of the welcome message.
+    {
+      lcd.scrollDisplayLeft();
+      delay(500);
+    }
+
+    lcd.clear();
+/////////////////****************END OF SETUP ROUTINE****************////////////////////////////
+}
+
+void loop()
+{
+  lcd.clear();
+
+  lcd.print(Word);
+  lcd.setCursor(0,0);
+  // Serial.print("Word: ");
+
+  // randomSeed(random());   //Seed for Random number Generator
+  // lcd.clear();
+  // lcd.setCursor(0,0);
+  // lcd.print("Word: ");
+
+  delay(1000);
+
+//Generate three random letters
+  firstLetter=generateLetter();
+  secondLetter=generateLetter();
+  thirdLetter=generateLetter();
+
+//Assign the random letters to the Word
+  Word[0]=firstLetter;
+  Word[1]=secondLetter;
+  Word[2]=thirdLetter;
+
+
+  // lcd.print(Word);
+  // lcd.setCursor(0,0);
+  Serial.print("Word: ");
+  lcd.setCursor(6,0);
+  Serial.println(Word);
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("Word exists?");
+  Serial.println("Word exists?");
+  delay(1000);
+
+  confirm();
+  delay(1000);
+
+  Serial.println("Continue?");
+  redo=Serial.readString();
+  delay(1000);
+
+  if (redo=="n"||redo=="N")
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Correct Words:");
+    lcd.setCursor(0, 1);
+    lcd.print(count);
+    Serial.print("Total number of correct Words: ");
+    Serial.println(count);
+    // break;
+  }
+
+  else;
+}
